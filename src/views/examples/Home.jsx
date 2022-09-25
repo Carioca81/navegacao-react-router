6b0f54b0c2a36287674425f5bad9e9d9b25b1eb7
Marcelo de Souza Pereira <marcelobct@gmail.com>
2022-09-25 13:00:50 -0300

    Terminada a seção sobre implementação de rotas com React Router

---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const Home = props => (
+    <div className="Início">
+        <h1>Componente Home</h1>
+        <h2>Bem vindo!</h2>
+    </div>
+)
+
+
+
+export default Home
